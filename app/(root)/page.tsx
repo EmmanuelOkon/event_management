55d7ca121dff7138bd36b7ed18f528f8a068bbf1
Emmanuel Okon <emmaudo27@gmail.com>
2024-04-15 20:15:00 +0100

    moved codebase over wsl

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -50,7 +50,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         className="wrapper my-8 flex flex-col gap-8 md:gap-12"
       >
         <h2 className="h2-bold">
-          Trust by <br /> Thousands of Events
+          Trust by <br /> Thousands of Events and go
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
